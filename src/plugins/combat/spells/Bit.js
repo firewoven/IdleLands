2fc14bda0e86a40fa5e53720603147723f2b43eb
firewoven <dan.aka.firewoven@gmail.com>
2017-03-17 09:35:56 -0400

    Update Bit.js

--- src/plugins/combat/spells/Bit.js
+++ src/plugins/combat/spells/Bit.js
@@ -3,6 +3,8 @@ import * as _ from 'lodash';
 import { Spell, SpellType } from '../spell';
 
 export class Bit extends Spell {
+  static description = 'A basic digital attack that uses INT to deal damage';
+  static element = SpellType.DIGITAL;
   static element = SpellType.DIGITAL;
   static stat = 'special';
   static tiers = [
